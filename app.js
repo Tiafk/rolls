8f3f7b9b9076b2fb97dcae42ba2b2cb8731acf59
Tiafk <akchurin227@mail.ru>
2025-06-04 05:28:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  loop: true,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 'auto',
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 2,
+//     },
+//     610: {
+//       slidesPerView: 2,
+//     },
+//     700: {
+//       slidesPerView: 3,
+//     },
+//     1020: {
+//       slidesPerView: 4,
+//     },
+//     1920: {
+//       slidesPerView: 5,
+//     },
+//   }
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 'auto',
+});
+
+var swiper2 = new Swiper(".mySwiper3", {
+  loop: true,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 'auto',
+});
